4869d23586a9a5c44116369def42a188e1b8a73c
Etienne Prothon <etienne.prothon@mypl.io>
2023-05-08 11:44:46 +0200

    feat: add gitui to gg keymap

--- mappings.lua
+++ mappings.lua
@@ -3,12 +3,14 @@
 -- Please use this mappings table to set keyboard mapping since this is the
 -- lower level configuration and more robust one. (which-key will
 -- automatically pick-up stored data by this setting.)
+local utils = require "astronvim.utils"
 return {
   -- first key is the mode
   n = {
     -- second key is the lefthand side of the map
     -- mappings seen under group name "Buffer"
     ["<leader>e"] = { "<cmd>Neotree toggle reveal<cr>", desc = "Toogle Explorer" },
+    ["<leader>gg"] = { function() utils.toggle_term_cmd "gitui" end, desc = "ToogleTerm gitui" },
     ["<leader>o"] = false,
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
     ["<leader>bD"] = {
